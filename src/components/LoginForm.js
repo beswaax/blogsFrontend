1869e8ae265ac9b36b06e7768eb7fd0f00c3a182
Samuel Clintoc <iroh99@protonmail.com>
2021-08-15 18:13:46 +0200

    added instruction to log in

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,42 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default ({ loginUser }) => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const submitHandler = async e => {
-        e.preventDefault();
+  const submitHandler = async (e) => {
+    e.preventDefault();
 
-        if (await loginUser({ username, password })) {
-            setUsername('')
-            setPassword('')      
-        }
-    };
+    if (await loginUser({ username, password })) {
+      setUsername("");
+      setPassword("");
+    }
+  };
 
-    return (
-        <section>
-            <h2>login</h2>
-            <form onSubmit={submitHandler}>
-                <div>
-                    <label htmlFor="login-username">username</label>
-                    <input 
-                        id="login-username" 
-                        type="text" 
-                        value={username}
-                        onChange={({ target }) => setUsername(target.value)} 
-                    />
-                </div>
-                <div>
-                    <label htmlFor="login-password">password</label>
-                    <input 
-                        id="login-password" 
-                        type="password" 
-                        value={password} 
-                        onChange={({ target }) => setPassword(target.value)} 
-                    />
-                </div>
-                <button type="submit">login</button>
-            </form>
-        </section>
-    )
-}
+  return (
+    <section>
+      <h4>username: baseAdmin | password: password</h4>
+      <h2>login</h2>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="login-username">username</label>
+          <input
+            id="login-username"
+            type="text"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="login-password">password</label>
+          <input
+            id="login-password"
+            type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </section>
+  );
+};
